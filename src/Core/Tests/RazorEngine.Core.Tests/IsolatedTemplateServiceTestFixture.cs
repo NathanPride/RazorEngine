8f8f7255568d7f48ac49a69a710389e050b62ac6
Nathan Pride <nathan@theprides.org>
2013-06-06 21:36:03 +0100

    Add extra test to check ParseToStream and RunToStream methods

--- src/Core/Tests/RazorEngine.Core.Tests/IsolatedTemplateServiceTestFixture.cs
+++ src/Core/Tests/RazorEngine.Core.Tests/IsolatedTemplateServiceTestFixture.cs
@@ -11,6 +11,7 @@
     using Compilation;
     using Templating;
     using TestTypes;
+    using System.IO;
 
     /// <summary>
     /// Defines a test fixture that provides tests for the <see cref="IsolatedTemplateService"/> type.
@@ -301,6 +302,101 @@
 
                 var results = service.ParseMany(templates, models, null, null, true);
                 Assert.That(expected.SequenceEqual(results), "Parsed templates do not match expected results.");
+            }
+        }
+
+        /// <summary>
+        /// Tests that a simple template without a model can be parsed.
+        /// </summary>
+        [Test]
+        public void IsolatedTemplateService_CanParseToStreamSimpleTemplate_WithNoModel()
+        {
+            using (var service = new IsolatedTemplateService())
+            using (var writer = new StringWriter())
+            {
+
+                const string template = "<h1>Hello World</h1>";
+                const string expected = template;
+
+                service.ParseToStream(writer, template, null, null, null);
+
+                string result = writer.ToString();
+
+                Assert.That(result == expected, "Result does not match expected: " + result);
+            }
+        }
+
+        /// <summary>
+        /// Tests that a simple template with a model can be parsed.
+        /// </summary>
+        [Test]
+        public void IsolatedTemplateService_CanParseToStreamSimpleTemplate_WithComplexSerializableModel()
+        {
+            using (var service = new IsolatedTemplateService())
+            using(var writer = new StringWriter())
+            {
+                const string template = "<h1>Hello @Model.Forename</h1>";
+                const string expected = "<h1>Hello Matt</h1>";
+
+                var model = new Person { Forename = "Matt" };
+
+                service.ParseToStream(writer, template, model, null, null);
+                string result = writer.ToString();
+
+                Assert.That(result == expected, "Result does not match expected: " + result);
+            }
+        }
+
+        /// <summary>
+        /// Tests that a simple template with html-encoding can be parsed.
+        /// </summary>
+        /// <remarks>
+        /// Text encoding is performed when writing objects to the template result (not literals). This test should 
+        /// show that the template service is correctly providing the appropriate encoding factory to process
+        /// the object's .ToString() and automatically encode it.
+        /// </remarks>
+        [Test]
+        public void IsolatedTemplateService_CanParseToStreamSimpleTemplate_UsingHtmlEncoding()
+        {
+
+            using (var service = new IsolatedTemplateService(Encoding.Html))
+            using (var writer = new StringWriter())
+            {
+                const string template = "<h1>Hello @Model.Forename</h1>";
+                const string expected = "<h1>Hello Matt &amp; World</h1>";
+
+                var model = new Person { Forename = "Matt & World" };
+
+                service.ParseToStream(writer, template, model, null, null);
+                string result = writer.ToString();
+
+                Assert.That(result == expected, "Result does not match expected: " + result);
+            }
+        }
+
+        /// <summary>
+        /// Tests that a simple template with no-encoding can be parsed.
+        /// </summary>
+        /// <remarks>
+        /// Text encoding is performed when writing objects to the template result (not literals). This test should 
+        /// show that the template service is correctly providing the appropriate encoding factory to process
+        /// the object's .ToString() and automatically encode it.
+        /// </remarks>
+        [Test]
+        public void IsolatedTemplateService_CanParseToStreamSimpleTemplate_UsingRawEncoding()
+        {
+            using (var service = new IsolatedTemplateService(Encoding.Raw))
+            using (var writer = new StringWriter())
+            {
+                const string template = "<h1>Hello @Model.Forename</h1>";
+                const string expected = "<h1>Hello Matt & World</h1>";
+
+                var model = new Person { Forename = "Matt & World" };
+
+                service.ParseToStream(writer, template, model, null, null);
+                string result = writer.ToString();
+
+                Assert.That(result == expected, "Result does not match expected: " + result);
             }
         }
         #endregion
